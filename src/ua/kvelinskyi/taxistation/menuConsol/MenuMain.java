a43a8bcd605834693af8eb163e26a0ee3cac42a4
Admin <IgorKvjava@gmail.ru>
2016-05-13 10:11:14 +0300

    ver 1.0

---
+++ src/ua/kvelinskyi/taxistation/menuConsol/MenuMain.java
@@ -0,0 +1,27 @@
+
+package ua.kvelinskyi.taxistation.menuConsol;
+
+import java.util.Scanner;
+
+public class MenuMain {
+    int choise;
+    public void prinMenuChoiseCars(){
+    System.out.println("Menu "
+                + "\nTo create a car park the following mark"
+                + "\n1 - BMW "
+                + "\n2 - Opel"+ "\n3 - Zaz"+ "\n4 - Fiat"+ "\n5 - Ford"+ "\n6 - Audi");
+    }
+    
+    public int choiseCars (int choise){       
+        switch (choise){
+            case 1:
+                
+                    break;                   
+            case 2:
+                
+                    break;
+        }
+        return choise;
+    }   
+    
+}
